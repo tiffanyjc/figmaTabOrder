c3507e27200a307c1016231ae55c3090cedd03c7
Tiffany Chen <tiffanyc1225@gmail.com>
2019-01-04 15:26:24 -0800

    commiting and then updating manifest with content script

--- tab-order-package/src/index.js
+++ tab-order-package/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/main.css';
 import * as serviceWorker from './serviceWorker';
 
 var numRows = 0; 
---
+++ tab-order-package/src/js/contentScript.js
@@ -0,0 +1,11 @@
+// "scripts": {
+//     // "build-rename": "npm run build-rename-js && npm run build-rename-css",
+//     // "build-rename-js": "renamer --regex --find 'main\\.[^\\.]+\\.js' --replace 'main.js' build/static/js/*.js",
+//     // "build-rename-css": "renamer --regex --find 'main\\.[^\\.]+\\.css' --replace 'main.css' build/static/css/*.css",
+
+//     "start": "react-scripts start",
+//     "build": "react-scripts build",
+//     // "build": "react-scripts build && npm run build-rename",
+//     "test": "react-scripts test",
+//     "eject": "react-scripts eject"
+//   },
